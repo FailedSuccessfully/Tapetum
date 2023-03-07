3440b8d8babb0323870dbafb380042cfa9cbe398
FailedSuccessfully <itay.fichman@gmail.com>
2023-03-07 13:30:01 +0200

    Create Collect_CalData.ps1

---
+++ Collect_CalData.ps1
@@ -0,0 +1,13 @@
+﻿$g
+$a
+$m
+$timeStart = Get-Date
+$timeNow = $timeStart
+$timeEnd = $timeStart.AddSeconds(30)
+$port= new-Object System.IO.Ports.SerialPort COM3,9600,None,8,one
+$port.Open()
+while ($timeNow -le $timeEnd){
+    Write-Host $port.ReadLine()
+    $timeNow = Get-Date
+}
+$port.Close()
